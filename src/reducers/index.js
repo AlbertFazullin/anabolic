5d858ffbe619d940984f019ad778c8a7ae95e4fc
Albert Fazullin <albert.fazullin@flatstack.com>
2017-03-30 11:36:10 +0300

    add middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import objectAssign from 'object-assign';
+
+import jwtDevReducer from '../lib/saveTokenReducer';
+
 import token from './token';
 import github from './github';
 import user from './user';
 
-export default objectAssign({}, token, github, user);
+export default objectAssign({}, { jwt: jwtDevReducer }, token, github, user);
